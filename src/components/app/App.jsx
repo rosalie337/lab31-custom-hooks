69397bd47b49d4cdc98ddc26adc4b02aae461d51
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-20 17:00:23 -0800

    npm i react-router-dom and moved Detail component to App.js

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom';
 import AllCharacters from '../../container/AllCharacters'
 import Header from '../header/Header';
+import Detail from '../details/Details';
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <AllCharacters />
-    </>
+    <Router>
+      <Route path="/" component={Header} />
+
+      <Route exact path="/" component={AllCharacters} />
+
+      <Route path="/:character" component={Detail} />
+    </Router>
   )
 }
 
--- src/container/AllCharacters.jsx
+++ src/container/AllCharacters.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import CharacterList from '../components/characters/CharacterList';
-import Details from '../components/details/Details';
 
 export default class AllCharacters extends Component {
     static propTypes = {
@@ -13,7 +12,6 @@ export default class AllCharacters extends Component {
             <div>
                 <h1>Hola from All Characters container!</h1>
                 <CharacterList />
-                <Details />
             </div>
         )
     }
